9d8d7cda88c4679dd036b632e4a166e4c4c82e84
LNMIG <migliorenicolas@yahoo.com.ar>
2023-07-18 22:43:44 -0300

    Fix: views and urls at tictactoe app

--- apps/tictactoe/urls.py
+++ apps/tictactoe/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 
+app_name = 'apps.tictactoe'
+
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('makemove', views.makemove, name='makemove'),
 ]
--- apps/tictactoe/views.py
+++ apps/tictactoe/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+tictactoe_initial = ['', '', '', '', 'X', '', '', '', '']
+tictactoe_final = []
 
 # Create your views here.
-def home(request):
-    return render(request, 'tictactoe/home.html')
+class HomeView(generic.ListView):
+    # global tictactoe_initial
+    # global tictactoe_final
+
+
+    template_name = 'tictactoe/home.html'
+    context_object_name = 'tictactoe'
+    
+    def get_queryset(self):
+        print(self.request)
+        print(tictactoe_initial)
+        print(tictactoe_final)
+        
+        if len(tictactoe_final)==0:
+            return tictactoe_initial
+        else:
+            return tictactoe_final
+
+def makemove(request):
+    global tictactoe_final
+    tictactoe_final = ['X', '', '', '', 'X', '', '', '', 'X']
+    square_selected = request.POST['square']
+    print(square_selected)
+    print(tictactoe_final)
+    return HttpResponseRedirect(reverse('apps.tictactoe:home', args=()))
