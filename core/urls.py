75ce33b64afe50803dee163a000c2c0f16d7c4ab
LNMIG <migliorenicolas@yahoo.com.ar>
2023-08-04 18:59:09 -0300

    Fiz: 404.html had errors

--- core/urls.py
+++ core/urls.py
@@ -18,11 +18,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler404
-from .views import NotFoundView
 
 from . import views
 
-handler404 = NotFoundView.as_view()
+handler404 = views.NotFoundView.as_view()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
