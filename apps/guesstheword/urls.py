02f05e0c011f64ab42a83e68cdf83b3d05ad70e6
LNMIG <migliorenicolas@yahoo.com.ar>
2023-08-04 18:50:09 -0300

    Fix: roots not working

--- apps/guesstheword/urls.py
+++ apps/guesstheword/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'apps.guesstheword'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('guesstheword/', views.HomeView.as_view(), name='home'),
     # path('makemove', views.makemove, name='makemove'),
     # path('reset', views.reset, name='resetgame'),
 ]
--- apps/tictactoe/urls.py
+++ apps/tictactoe/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'apps.tictactoe'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('makemove', views.makemove, name='makemove'),
-    path('reset', views.reset, name='resetgame'),
+    path('tictactoe/', views.HomeView.as_view(), name='home'),
+    path('tictactoe/makemove/', views.makemove, name='makemove'),
+    path('tictactoe/reset/', views.reset, name='resetgame'),
 ]
