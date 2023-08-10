905924cb718e14878284a95f3f50ef17ec0af9b5
LNMIG <migliorenicolas@yahoo.com.ar>
2023-08-10 15:14:13 -0300

    Add: basic dictionary and keyboard's keys

---
+++ apps/guesstheword/keyboard.py
@@ -0,0 +1,7 @@
+def keyboard_keys():
+    guesstheword_keyboard = [
+        ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+        ['A', 'S', 'D', 'F','G', 'H', 'J', 'K', 'L'],
+        ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+    ]
+    return guesstheword_keyboard
